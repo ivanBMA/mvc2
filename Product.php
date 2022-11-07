3cf5baa063de56f04879c2b8f48dbc9aed88ffe7
ivanMoros <ivanmprp@gmail.com>
2022-11-07 16:30:53 +0000

    clase mvc

---
+++ Product.php
@@ -0,0 +1,23 @@
+<?php
+    class Product{
+        const PRODUCTS = [
+          array(1,'cortacesped'),
+          array(2,'Pizarra'),
+          array(3,'Billar'),
+          array(4,'Dardos'),
+        ];
+
+        function __construct(){
+            //Contructor vaio
+        }
+        //Devuelve todos los productos
+        public static function all(){
+        return Product::PRODUCTS;
+        }
+        
+        //Devolcer un producto en particular
+        public function find($id){
+          return Product::PRODUCTS[$id-1];
+        }
+
+    }//fin clase
