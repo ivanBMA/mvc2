570c1e820af0ba4795270baf5236e2dd56a59d77
ivanMoros <ivanmprp@gmail.com>
2022-11-10 15:58:12 +0000

    clase 16:58 10/11/2022

---
+++ core/App.php
@@ -0,0 +1,52 @@
+<?php
+
+    /*
+        -Si no la url no especifica ningun controlador (recurso) => asigno
+        uno por defecto => home
+        -Si no la url no especifica ninguno metodo => asigno por defecto: index
+    */
+
+    class App{
+        function __construct(){
+            //http://mvc.local/product/show => http://mvc.local/index.php?url=product/show
+            
+            if(isset($_GET["url"]) && !empty($_GET["url"])){
+                $url = $_GET["url"];
+            }else{
+                $url = "home";
+            }
+            // /product/show -> product: recurso; show: accion; 5: parametro
+            $arguments = explode("/", trim($url, "/"));
+            $controllerName = array_shift($arguments);//Product ; ProductController
+            $controllerName = ucwords($controllerName) . "Controller";
+            
+            if(count($arguments) > 0){//<----------------)-___-
+                $method = array_shift($arguments);//Show
+            }else{
+                $method = "index";
+            }
+
+            //Voy a cargar el controlador. ProductController.php
+            $file = "app/controllers/$controllerName" . ".php";
+            if(file_exists($file)){
+                require_once $file;//importo el fichero si existe
+            }else{
+                http_response_code(404);
+                die("No encontrado");
+            }
+
+            //Exite el metodo en el controlador ?
+            $controllerObject = new $controllerName();//Objeto de la clase
+            if(method_exists($controllerObject,$method)){
+                $controllerObject->$method($arguments);//metodo ok -> lo invoco.
+            }else{
+                http_response_code(404);
+                die("No encontrado");
+            }
+
+
+
+
+        }//fin_contruct
+
+    }//fin_app
--- start.php
+++ start.php
@@ -1,30 +1,4 @@
 <?php
-    echo "Privado";
-    //Recurso/accion/parametro
-      //Recurso : controladores
-      //Accion: metodos del controladores (controlador->show())
-      //Parametros : find -> id de producto.
-    require_once "Controller.php";
-      $app = new Controller();
-      //$controller->home();
-      //$controler->show();
+    require_once "core/App.php";
 
-      //Recoger el metodo que pasan como parametro y si no
-      // esoecifican nigun metodo cargar el metodo home
-      if(isset($_GET["method"])){
-          $method = $_GET["method"];//Show ,find,create, update...
-      }else{
-          $method = "home";
-      }
-
-      //2 - Verificar que el metodo introducido existe.
-      if(method_exists($app, $method)){
-          $app->$method();
-      }else{
-          http_response_code(404);
-          die("Metodo no encontrado");//exit
-      }
-
-
-
-      $app->method;
+    $app = new App();
